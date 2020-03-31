b73d40e0cdafb7c7a4ac1e8d0374deebfc17229f
Yoko <baghram0001@gmail.com>
2020-03-31 19:29:11 +0700

    Routes project

--- routes/project.js
+++ routes/project.js
@@ -1,4 +1,15 @@
 const projectRoutes = require('express').Router()
+const Controller = require('../controller/controller')
+
+projectRoutes.get('/', Controller.getProject)
+projectRoutes.post('/add', Controller.addProject)
+projectRoutes.delete('/delete/:id', Controller.deleteProject)
+projectRoutes.post('/friend', Controller.addFriend)
+projectRoutes.delete('/friend/:id', Controller.deleteFriend)
+projectRoutes.get('/todos', Controller.getTodo)
+projectRoutes.post('/todos', Controller.addTodo)
+projectRoutes.patch('/todos/:id', Controller.updateTodo)
+projectRoutes.delete('/todos/:id', Controller.deleteTodo)
 
 
 module.exports = projectRoutes
